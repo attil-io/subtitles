70ef06c130d1c1a234047b4eb2dde76ceb990ec7
A <parrotchaffinch@gmail.com>
2018-09-08 11:40:39 -0400

    Add api url

--- be/index.js
+++ be/index.js
@@ -5,12 +5,5 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.send('Hello world'))
-  .get('/api/result', (req, res) => {
-      const result = {
-	      field: 42,
-	      another: 43
-      }
-      res.json(result)
-  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
