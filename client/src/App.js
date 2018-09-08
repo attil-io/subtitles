2d6a56e672e10eb5d3f90020c812bf2b2d853c20
A <parrotchaffinch@gmail.com>
2018-09-08 17:10:16 -0400

    TextStepper ui side

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import socketIOClient from "socket.io-client";
 import './App.css';
 import TextDisplay from './TextDisplay.js'
-import TextStepper from './TextCreator.js'
+import TextStepper from './TextStepper.js'
 
 
 class App extends Component {
--- client/src/TextStepper.js
+++ client/src/TextStepper.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 import socketIOClient from "socket.io-client";
+import "./TextStepper.css"
 
+class Line extends Component {
+  render() {
+	  return (
+	  <div className={this.props.active ? 'activeLine' : 'inactiveLine'}>{this.props.active ? "CURRENTLY DISPLAYED: " : ""}<p>{this.props.text}</p> </div>
+	  )
+  }
+}
+
+class Chooser extends Component {
+  render () {
+         return (
+		 <div><select onChange={this.props.onchange}>
+  			<option value="volvo">Volvo</option>
+  			<option value="saab">Saab</option>
+  			<option value="mercedes">Mercedes</option>
+  			<option value="audi">Audi</option>
+		</select></div>
+	 )
+  }
+}
 
 class TextStepper extends Component {
+
+  state = { text: [], activeIdx : 0 }
+
+  chooserOnChange(evt) {
+    console.log(evt.target.value)
+  }
+
+
+  backOnClick(evt) {
+    console.log("back")
+  }
+
+  forwardOnClick(evt) {
+    console.log("forward")
+  }
+
+
   render() {
     return (
       <div>
-	    ID <input type="text" />
-	    <br />
-	    name <input type="text" />
+	    <Chooser onchange={this.chooserOnChange.bind(this)} />
+	    <Line active={false} text="before1" />
+	    <Line active={false} text="before2" />
+	    <Line active={true} text="active line" />
+	    <Line active={false} text="after1" />
+	    <Line active={false} text="after2" />
+
+	    <div>
+	    	<input type="button" value="&lt;" onClick={this.backOnClick.bind(this)}/>
+	    	<input type="button" value="&gt;" onClick={this.forwardOnClick.bind(this)}/>
+	    </div>
       </div>
     );
   }
