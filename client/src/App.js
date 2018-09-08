416a9f21110c5bd7c0a4ad057e06b6c7728ece58
A <parrotchaffinch@gmail.com>
2018-09-08 13:21:46 -0400

    Query the result from the API

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = { result: {} }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getResult();
+  }
+
+  getResult = () => {
+    // Get the passwords and store them in state
+    fetch('/api/result')
+      .then(res => res.json())
+      .then(result => this.setState({ result }));
+  }
+
   render() {
     return (
       <div className="App">
-	    Hello world app.
+	    Result is: { this.state.result.field }
       </div>
     );
   }
