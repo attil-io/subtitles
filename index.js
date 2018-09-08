35b40e09ab494f28b1df983fb870779ad1db53ea
A <parrotchaffinch@gmail.com>
2018-09-08 11:04:56 -0400

    world

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send('Hello'))
+  .get('/', (req, res) => res.send('Hello world'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
